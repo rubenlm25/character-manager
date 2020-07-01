eb1926c0a2092bc98880ac289a28854ce9b6b193
rubenlm25 <ruben.lomomaestu@gmail.com>
2020-07-01 14:22:35 +0200

    fonction put faites

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -1,30 +1,51 @@
-dataidJSON = localStorage.getItem('idedit');
-dataid = dataidJSON && JSON.parse(dataidJSON);
-let inputname;
-let inputshortdescription;
-let inputdescritpion;
+import 'regenerator-runtime/runtime'
+let dataidJSON = localStorage.getItem('idedit');
+let dataid = dataidJSON && JSON.parse(dataidJSON);
+console.log(dataid);
+// let inputname;
+// let inputshortdescription;
+// let inputdescritpion;
 let jsonuser;
+let datas;
 //get the user
 const axios = require('axios');
 async function makeGetRequest() {
 
-    let res = await axios.get('http://localhost:3000/users/'+dataid);
-    let data = res.data;
-    console.log(data);
-    return(data);
-  }
-  jsonuser=makeGetRequest();
-////////////////////////////////////////////////////////////////////////////////
-// complete with actual user in input and text area
-document.getElementById("edit-description").innerHTML = jsonuser.descritpion;
-document.getElementById("edit-shortdescription").innerHTML = jsonuser.shortDescription;
-document.getElementById("edit-name").value = jsonuser.name;
-///////////////////////////////////////////////////////////////////////////////////////////
+  let res = await axios.get('https://character-database.becode.xyz/characters/' + dataid);
+  datas = res.data;
+  console.log(datas);
+  
+  
+  ////////////////////////////////////////////////////////////////////////////////
+  // complete with actual user in input and text area
+  document.getElementById("edit-description").innerHTML = datas.description;
+  document.getElementById("edit-shortdescription").innerHTML = datas.shortDescription;
+  document.getElementById("edit-name").value = datas.name;
+  ///////////////////////////////////////////////////////////////////////////////////////////
+}
+makeGetRequest();
 //clic for edit
-document.getElementById("button-edit").addEventListener("click",async()=>{
-    inputname=document.getElementById("edit-name").value;
-    inputdescritpion=document.getElementById("edit-description").innerHTML;
-    inputshortdescription = document.getElementById("edit-shortdescription").innerHTML;
+document.getElementById("button-edit").addEventListener("click", async () => {
+  let inputname = document.getElementById("edit-name").value;
+  let inputdescription = document.getElementById("edit-description").value;
+  let inputshortdescription = document.getElementById("edit-shortdescription").value;
 
-});
-/////////////////////////////////////////////////
+
+
+  editcharacter(inputname, inputdescription, inputshortdescription);
+
+  // verification all value complete
+  async function editcharacter(name, description, shortdescription) {
+    let params = {
+          name: name,
+          shortDescription: shortdescription,
+          description: description
+        }
+        let res = await axios.put('https://character-database.becode.xyz/characters/'+dataid, params);
+        console.log(res.data);
+        window.location = "./index.html";
+      }     
+ 
+      
+    });
+    /////////////////////////////////////////////////
